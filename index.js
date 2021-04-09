6a8a4057d771dc2ed7328f8f20c3b40ec082ae97
Zach Frohardt <zfrohardt@gmail.com>
2021-04-09 12:02:44 -0700

    working code, but errors persist because of unknown test formatting

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(name, email) {
+    let userData = {
+        "name": name,
+        "email": email,
+    };
+    let fetchVar = fetch("http://localhost:3000/users", getConfigObject(userData));
+
+    fetchVar.then(response => response.json())
+    .then(json => document.body.append(json.id + "\n"))
+    .catch(error => {
+        document.body.append(error.message);
+    });
+
+    return fetchVar;
+}
+
+function getConfigObject(reqBody) {
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+    };
+
+    configObj.body = JSON.stringify(reqBody);
+
+    return configObj;
+}
